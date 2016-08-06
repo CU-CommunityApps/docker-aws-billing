c4b5ceb2bf6202393f7ac781581fb26dbdba06a9
sbower <shawn.bower@gmail.com>
2016-08-05 23:00:16 -0400

    changes to add application and caluclate month integer

--- aws_detail_billing_mysql.rb
+++ aws_detail_billing_mysql.rb
@@ -22,7 +22,7 @@ MYSQL_DB = ENV['MYSQL_DB'] || "billing_detail"
 
 S3_BUCKET = ENV['S3_BUCKET']
 
-BILLING_DETAIL_INSERT = "INSERT INTO billing_detail (invoice_id, payer_account_id, linked_account_id, record_type, record_id, product_name, rate_id, subscription_id, pricing_plan_id, usage_type, operation, availability_zone, reserved_instance, item_description, usage_start_date, usage_end_date, usage_quantity, blended_rate, blended_cost, unblended_rate, unblended_cost, resource_id, cost_center) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+BILLING_DETAIL_INSERT = "INSERT INTO billing_detail (invoice_id, payer_account_id, linked_account_id, record_type, record_id, product_name, rate_id, subscription_id, pricing_plan_id, usage_type, operation, availability_zone, reserved_instance, item_description, usage_start_date, usage_end_date, usage_quantity, blended_rate, blended_cost, unblended_rate, unblended_cost, resource_id, cost_center, application, month, year) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
 puts "start: #{start_month} end: #{end_month} year: #{year}"
 
 if start_month > end_month then
@@ -55,10 +55,12 @@ begin
   puts "beginning to process CSV file"
   CSV.foreach(File.basename(zip_file, '.zip')) do |row|
     if (row[0] and row[1] and row[3]) and (not row[0].eql?("InvoiceID"))
+      usage_start_date = Date.parse(row[15])
       st = db.prepare(BILLING_DETAIL_INSERT)
       st.execute(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12],row[13],row[14],row[15],
-        row[16],row[17],row[18],row[19],row[20],row[21],row[23])
+        row[16],row[17],row[18],row[19],row[20],row[21],row[23],row[22],usage_start_date.month, usage_start_date.year)
       st.close
+      exit 0
     end
   end
 
